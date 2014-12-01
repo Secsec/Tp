bbb2a2ad3dca62abee0c3c33d7101c065d3eadc8
Secsec <vincent_bronikowski@hotmail.fr>
2014-12-01 10:21:01 +0100

    Commit du code

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char[] * argv){
+int i;	// déclaration de variable
+printf("Coucou ! :))) \n"); // ligne qui sert à rien
+for(i=0:i<10;i++)  // boucle qui sert à rien
+{
+	printf("%d\n",i);
+}
+printf("Fin du programme"); // ligne qui sert à rien
+return 0; //fin
+}
